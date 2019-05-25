d0e03df0574096fff264efaf2449baf4fdfad05f
Arash Tadjiki <atadjiki@yahoo.com>
2019-05-24 18:03:53 -0600

    Now have a collection of peds

--- ArashsMod/Streetwar.cs
+++ ArashsMod/Streetwar.cs
@@ -8,82 +8,111 @@ namespace Mod
 {
     public class Streetwar : Script
     {
+        private bool debug = true;
+        private int debugTime = 4000;
         private bool scripton = false;
         private bool revent = false;
 
-        private string[] CopSkins = { "M_Y_COP", "M_Y_SWAT", "M_M_FBI", "M_M_FATCOP_01" };
-        private string[] CriminalSkins = { "M_Y_GOON_01", "M_Y_THIEF", "M_Y_GRU2_LO_01", "M_M_GRU2_LO_02", "M_M_GRU2_HI_02", "M_M_GRU2_HI_01",
+        private string[] CopSkins = { "M_Y_SWAT" };
+        private string[] CriminalSkins = { "M_Y_GRU2_LO_01", "M_M_GRU2_LO_02", "M_M_GRU2_HI_02", "M_M_GRU2_HI_01",
                                            "M_Y_GRUS_HI_02", "M_Y_GRUS_LO_02", "M_Y_GRUS_LO_01", "M_O_GRUS_HI_01", "M_Y_GALB_LO_01", "M_Y_GALB_LO_02",
-                                           "M_Y_GALB_LO_03", "M_Y_GALB_LO_04", "M_Y_GMAF_HI_01", "M_Y_GMAF_HI_02", "M_M_FATMOB_01" };
+                                           "M_Y_GALB_LO_03", "M_Y_GALB_LO_04" };
 
-        private const int maxTotalPeds = 32;
-        private const int maxSpawnDuringTick = 5;
-        private int spawnRadius = 50;
-        private int fightRadius = 300;
-        public List<Ped> peds;
+        private const int interval = 10000;
+        private const int spawnRadius = 25;
+        private const int fightRadius = 200;
+
+        private const int cops = 8;
+        private const int robbers = 16;
+
+        private const int copHealth = 100;
+        private const int robberHealth = 50;
+
+        private const int wavesUntilCops = 3;
+
+        private int currentWave = 0;
+        private const int MaxPeds = 64;
+        private int totalSpawned = 0;
+
+        PedCollection peds = new PedCollection();
+
+        public enum WeaponTier { Melee, Pistols, Full };
+        WeaponTier weaponTier = WeaponTier.Melee;
 
         public Streetwar()
         {
             //set interval
-            Interval = Settings.GetValueInteger("INTERVAL", "SETTINGS", 10000);
+            Interval = Settings.GetValueInteger("INTERVAL", "SETTINGS", interval);
             BindKey(Settings.GetValueKey("Toggle Script", "SETTINGS", Keys.K), new KeyPressDelegate(ScriptOn));
             BindKey(Settings.GetValueKey("Toggle Pedestrian Events", "SETTINGS", Keys.L), new KeyPressDelegate(RandomEventsPedestrians));
+            BindKey(Settings.GetValueKey("Toggle Debug", "SETTINGS", Keys.End), new KeyPressDelegate(ToggleDebug));
 
             //bind tick event
             this.Tick += new EventHandler(RandomPedEvents_Tick);
 
-            peds = new List<Ped>();
+        }
 
+        public void ToggleDebug()
+        {
+            if (debug)
+            {
+                debug = false;
+            }
+            else
+            {
+                debug = true;
+            }
         }
         private void SpawnPedestrian(bool cop)
         {
-
-            if (peds.Count >= maxTotalPeds)
-            {
-                foreach (Ped ped in peds)
-                {
-                    if (ped.isAlive == false)
-                    {
-                        peds.Remove(ped);
-                    }
-                }
-
-                return;
-            }
 
             int mod = 1;
 
             if (!cop) { mod *= -1; }
 
-            for (int i = 0; i < RandomNumber(0, maxSpawnDuringTick); i++)
+            Ped ped = World.CreatePed(RandomModel(cop), Player.Character.GetOffsetPosition(new Vector3(mod * RandomNumber(spawnRadius / 2, spawnRadius), mod * RandomNumber(spawnRadius / 2, spawnRadius), 0.0f)).ToGround());
+            if (Exists(ped) && ped != null)
             {
-                Ped ped = World.CreatePed(RandomModel(cop), Player.Character.GetOffsetPosition(new Vector3(mod * spawnRadius, spawnRadius, 0.0f)).ToGround());
-                if (Exists(ped) && ped != null)
+                ped.BecomeMissionCharacter();
+
+                if (cop)
                 {
-                    ped.BecomeMissionCharacter();
+                    ped.RelationshipGroup = RelationshipGroup.Cop;
+                    ped.ChangeRelationship(RelationshipGroup.Criminal, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Dealer, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Player, Relationship.Respect);
 
-                    if (cop)
-                    {
-                        ped.RelationshipGroup = RelationshipGroup.Cop;
-                        ped.ChangeRelationship(RelationshipGroup.Criminal, Relationship.Hate);
-                    }
-                    else
-                    {
-                        ped.RelationshipGroup = RelationshipGroup.Criminal;
-                        ped.ChangeRelationship(RelationshipGroup.Cop, Relationship.Hate);
-                    }
-
-                    ped.Task.FightAgainstHatedTargets(300);
-                    ped.MaxHealth = 100;
-                    ped.Health = 100;
+                    ped.MaxHealth = copHealth;
+                    ped.Health = copHealth;
                     ped.Armor = 50;
-                    RandomWeapons(ped, cop);
 
-                    peds.Add(ped);
-
-                    Wait(10);
                 }
+                else
+                {
+                    ped.RelationshipGroup = RelationshipGroup.Criminal;
+                    ped.ChangeRelationship(RelationshipGroup.Cop, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Civillian_Male, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Civillian_Female, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Fireman, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Bum, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Medic, Relationship.Hate);
+                    ped.ChangeRelationship(RelationshipGroup.Player, Relationship.Neutral);
+
+                    ped.MaxHealth = robberHealth;
+                    ped.Health = robberHealth;
+                    ped.Armor = 0;
+                    ped.WantedByPolice = true;
+                    ped.StartKillingSpree(true);
+                }
+
+                ped.Task.FightAgainstHatedTargets(fightRadius);
+
+                RandomWeapons(ped, cop);
+                peds.Add(ped);
+                totalSpawned++;
+                Wait(10);
             }
+
         }
         private int RandomNumber(int min, int max)
         {
@@ -97,41 +126,132 @@ namespace Mod
                 revent = !revent;
                 if (revent)
                 {
-                    Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Streetwar Starting...", 4000, 1);
+                    if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Streetwar Starting...", debugTime, 1);
                 }
                 else
                 {
-                    Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Streetwar Over", 4000, 1);
+                    if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Streetwar Over. Total spawned: " + totalSpawned, debugTime, 1);
+                    Wait(1000);
+                    if (Player.CanControlCharacter == false)
+                    {
+                        int pedsKilled = 0;
+                        foreach (Ped ped in peds)
+                        {
+                            if (ped.Exists())
+                            {
+                                ped.Die();
+                                pedsKilled++;
+
+                            }
+                        }
+                        if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Killed off " + pedsKilled + " peds", debugTime, 1);
+                        peds.Clear();
+                    }
+                    currentWave = 0;
                 }
             }
         }
+
         public void RandomPedEvents_Tick(object sender, EventArgs e)
         {
+            if (Player.CanControlCharacter == false)
+            {
+                if (peds.Count > 0)
+                {
+                    peds.Clear();
+                }
+
+                RandomEventsPedestrians();
+
+                return;
+            }
             if (scripton)
             {
                 if (revent)
                 {
+
+                    if (peds.Count >= MaxPeds)
+                    {
+                        int currentSize = peds.Count;
+                        List<Ped> toRemove = new List<Ped>();
+
+                        foreach (Ped ped in peds)
+                        {
+                            if (ped.Exists() == false)
+                            {
+                                toRemove.Add(ped);
+                            }
+                            else if (ped.isDead)
+                            {
+                                toRemove.Add(ped);
+                            }
+
+                        }
+
+                        foreach (Ped ped in toRemove)
+                        {
+                            peds.Remove(ped);
+                        }
+
+                        if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Hit max amount of peds, removed " + (currentSize - peds.Count) + " peds - Total: " + peds.Count, debugTime, 1);
+                        return;
+                    }
+
                     if (Player.Character.isInVehicle() == false)
                     {
-                        int faction = RandomNumber(0, 10);
+                        if (currentWave >= wavesUntilCops)
+                        {
+                            currentWave = 0;
 
-                        if (faction <= 5)
-                        {
-                            SpawnPedestrian(true);
+                            for (int i = 0; i < cops; i++)
+                            {
+                                SpawnPedestrian(true);
+                            }
+                            if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Spawned " + cops + " police. Total: " + peds.Count, debugTime, 1);
                         }
-                        else if (faction > 5)
+                        else
                         {
-                            SpawnPedestrian(false);
+                            int amountOfRobbers = RandomNumber(robbers / 2, robbers);
+                            int currentSize = peds.Count;
+
+                            for (int i = 0; i < RandomNumber(cops, robbers); i++)
+                            {
+                                SpawnPedestrian(false);
+                            }
+                            RandomWeaponTier();
+
+                            if (debug) Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", "STRING", "Spawned " + (peds.Count - currentSize) + " " + weaponTier.ToString() + " criminals. Wave " + (currentWave + 1) + " Total: " + peds.Count, debugTime, 1);
+                            currentWave++;
                         }
                     }
                 }
             }
+
         }
+
+        public void RandomWeaponTier()
+        {
+            int tier = RandomNumber(0, 3);
+
+            if (tier == 0)
+            {
+                weaponTier = WeaponTier.Melee;
+            }
+            else if (tier == 1)
+            {
+                weaponTier = WeaponTier.Pistols;
+            }
+            else if (tier == 2)
+            {
+                weaponTier = WeaponTier.Full;
+            }
+        }
+
         public void RandomWeapons(Ped ped, bool cop)
         {
             if (cop)
             {
-                int weapon = RandomNumber(0, 4);
+                int weapon = RandomNumber(0, 5);
 
                 if (weapon == 0)
                 {
@@ -149,35 +269,77 @@ namespace Mod
                 {
                     ped.Weapons.BarettaShotgun.Ammo = 999999;
                 }
+                else if (weapon == 4)
+                {
+                    ped.Weapons.SniperRifle_M40A1.Ammo = 999999;
+                }
             }
             else
             {
-                int weapon = RandomNumber(0, 6);
+                if (weaponTier == WeaponTier.Melee)
+                {
+                    int weapon = RandomNumber(0, 10);
 
-                if (weapon == 0)
-                {
-                    ped.Weapons.AssaultRifle_AK47.Ammo = 999999;
+                    if (weapon < 9 && weapon > 3)
+                    {
+                        ped.Weapons.Knife.Ammo = 1;
+                    }
+                    else if (weapon < 9 && weapon > 0)
+                    {
+                        ped.Weapons.BaseballBat.Ammo = 1;
+                    }
+                    else
+                    {
+                        ped.Weapons.MolotovCocktails.Ammo = 4;
+                    }
+
                 }
-                else if (weapon == 1)
+                else if (weaponTier == WeaponTier.Pistols)
                 {
-                    ped.Weapons.Uzi.Ammo = 999999;
+                    int weapon = RandomNumber(0, 2);
+
+                    if (weapon == 0)
+                    {
+                        ped.Weapons.Glock.Ammo = 999999;
+                    }
+                    else if (weapon == 1)
+                    {
+                        ped.Weapons.DesertEagle.Ammo = 999999;
+                    }
                 }
-                else if (weapon == 2)
+                else if (weaponTier == WeaponTier.Full)
                 {
-                    ped.Weapons.Glock.Ammo = 999999;
+                    int weapon = RandomNumber(0, 6);
+
+                    if (weapon == 0)
+                    {
+                        ped.Weapons.AssaultRifle_AK47.Ammo = 999999;
+                    }
+                    else if (weapon == 1)
+                    {
+                        ped.Weapons.Uzi.Ammo = 999999;
+                    }
+                    else if (weapon == 2)
+                    {
+                        ped.Weapons.Glock.Ammo = 999999;
+                    }
+                    else if (weapon == 3)
+                    {
+                        ped.Weapons.BasicShotgun.Ammo = 999999;
+                    }
+                    else if (weapon == 4)
+                    {
+                        ped.Weapons.MolotovCocktails.Ammo = 999999;
+
+                    }
+                    else if (weapon == 5)
+                    {
+                        ped.Weapons.AnySniperRifle.Ammo = 99999;
+
+                    }
                 }
-                else if (weapon == 3)
-                {
-                    ped.Weapons.BasicShotgun.Ammo = 999999;
-                }
-                else if (weapon == 4)
-                {
-                    ped.Weapons.MolotovCocktails.Ammo = 10;
-                }
-                else if (weapon == 5)
-                {
-                    ped.Weapons.RocketLauncher.Ammo = 10;
-                }
+
+
             }
 
         }
